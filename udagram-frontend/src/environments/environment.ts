a9f51ae6f0c3f119c5c9d05c07c8af2faf2ea867
Hamada Elbassel <Hamadaelbassel@gmail.com>
2022-05-15 16:21:38 +0200

    ..

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost: "Udagramapi-env.eba-pr5i6m9q.us-east-1.elasticbeanstalk.com",
 };
 
 /*
