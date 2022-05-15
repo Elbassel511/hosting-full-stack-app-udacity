3fc3e9263cd11bbb65882b6483badedbb675a864
Hamada Elbassel <Hamadaelbassel@gmail.com>
2022-05-16 01:08:21 +0200

    url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: "Udagram",
-  apiHost: "http://udagram.eba-pr5i6m9q.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://udagram.eba-pr5i6m9q.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
