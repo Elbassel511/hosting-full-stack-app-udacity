8cb6b2475bb14524ed8935452dbd2ef38fb642b7
Hamada Elbassel <Hamadaelbassel@gmail.com>
2022-05-17 17:24:05 +0200

    aws config

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -3,6 +3,15 @@ import { config } from "./config/config";
 
 //Credentials are auto set according to the documentation https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html and the default profile is "Default anyway"
 
+console.log(config.aws_media_bucket, config.aws_region);
+
+AWS.config = new AWS.config({
+  accessKeyId: config.aws_access_key,
+  secretAccessKey: config.aws_secret_access_key,
+  region: config.aws_region,
+  signatureVersion: "v4",
+});
+
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
@@ -12,21 +21,24 @@ export const s3 = new AWS.S3({
 // Generates an AWS signed URL for retrieving objects
 export function getGetSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
-
-  return s3.getSignedUrl("getObject", {
+  const signedUrl = s3.getSignedUrl("getObject", {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
+  console.log(signedUrl);
+  return signedUrl;
 }
 
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
-
-  return s3.getSignedUrl("putObject", {
+  const signedUrl = s3.getSignedUrl("putObject", {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
+  console.log(signedUrl);
+
+  return signedUrl;
 }
